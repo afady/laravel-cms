baaf942c140b0c21d170a7f7a6c25d56c2efbc86
Fady Farid <fadyfarid55@gmail.com>
2018-08-16 15:16:04 -0400

    initial commit

---
+++ database/migrations/2018_08_15_204958_create_categories_table.php
@@ -0,0 +1,46 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCategoriesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('categories', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('title')->unique()->required();
+            $table->string('slug')->unique()->required();
+            $table->timestamps();
+
+            $table->softDeletes();
+        });
+
+        Schema::create('category_post', function (Blueprint $table) {
+            $table->unsignedInteger('category_id')->required();
+            $table->unsignedInteger('post_id')->required();
+            $table->timestamps();
+
+            $table->softDeletes();
+            $table->unique(['category_id', 'post_id']);
+            $table->foreign('category_id')->references('id')->on('categories')->onDelete('cascade');
+            $table->foreign('post_id')->references('id')->on('posts')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('categories');
+    }
+}
